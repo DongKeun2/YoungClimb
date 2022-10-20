03037867b6e93376de69fd9a953955557c3621e0
DK <ehdrms121@naver.com>
2022-10-20 15:34:11 +0900

    feat: add initialscreen background image

--- FrontEnd/screens/InitialScreen.js
+++ FrontEnd/screens/InitialScreen.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useRef, useEffect} from 'react';
+import {ImageBackground, Animated, View, Text} from 'react-native';
+import background from '../assets/image/initial/background.png';
 
 function InitialScreen() {
+  const fadeAnim = useRef(new Animated.Value(1)).current;
+
+  // const fadeIn = () => {
+  //   Animated.timing(fadeAnim, {
+  //     toValue: 1,
+  //     duration: 1000,
+  //     useNativeDriver: true,
+  //   }).start();
+  // };
+
+  const fadeOut = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  useEffect(() => {
+    // fadeIn();
+    setTimeout(() => {
+      fadeOut();
+    }, 2000);
+  });
+
   return (
-    <View>
-      <Text>초기화면!</Text>
-    </View>
+    <Animated.View
+      style={{
+        opacity: fadeAnim,
+      }}>
+      <ImageBackground
+        source={background}
+        style={{width: '100%', height: '100%'}}>
+        <></>
+      </ImageBackground>
+    </Animated.View>
   );
 }
 
