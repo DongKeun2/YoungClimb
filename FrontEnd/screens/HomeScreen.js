b6c47c1a3d0b1a7b84194607ca0a08c371ea50d1
DK <ehdrms121@naver.com>
2022-10-20 09:58:47 +0900

    feat: create tabnavigator

---
+++ FrontEnd/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function HomeScreen() {
+  return (
+    <>
+      <Text>Home!</Text>
+    </>
+  );
+}
+
+export default HomeScreen;
