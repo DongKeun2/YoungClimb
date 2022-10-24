c83b2731ccc01265d57761914747b5c5fac6a2ee
DK <ehdrms121@naver.com>
2022-10-24 13:12:31 +0900

    feat: update image for main

--- FrontEnd/screens/MainScreen.js
+++ FrontEnd/screens/MainScreen.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Image} from 'react-native';
 import CustomButton from '../components/CustomBtn';
+
+import logo from '../assets/image/main/logo.png';
+import title from '../assets/image/main/title.png';
 
 function MainScreen({navigation}) {
   return (
     <View style={styles.container}>
-      <View style={styles.logo} />
+      <View style={styles.header}>
+        <Image source={logo} style={styles.logo} />
+
+        <Image source={title} style={styles.title} />
+      </View>
       <View style={styles.btnGroup}>
         <CustomButton
           buttonColor="#EF3F8F"
@@ -30,9 +37,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  logo: {
+  header: {
+    justifyContent: 'center',
+    alignItems: 'center',
     width: '100%',
     height: '50%',
+  },
+  logo: {},
+  title: {
+    width: '100%',
+
+    resizeMode: 'contain',
   },
   btnGroup: {
     width: '80%',
