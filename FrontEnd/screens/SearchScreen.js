32bada35935de7e0dd224e6b4807010c64e8450b
DK <ehdrms121@naver.com>
2022-11-02 17:13:14 +0900

    feat: 게시글 검색 임시 UI

--- FrontEnd/screens/SearchScreen.js
+++ FrontEnd/screens/SearchScreen.js
@@ -1,9 +1,11 @@
-import React, {useState, useRef} from 'react';
+import React, {useState} from 'react';
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {Picker} from '@react-native-picker/picker';
 
 import CustomMainHeader from '../components/CustomMainHeader';
+
+import {holdList} from '../assets/info/ColorInfo';
 
 import SearchBtnIcon from '../assets/image/search/searchBtn.svg';
 import UserIcon from '../assets/image/search/user.svg';
@@ -58,9 +60,14 @@ function SearchScreen({navigation}) {
 }
 
 function BoardTab({navigation}) {
-  const [isCheck, setIsCheck] = useState(false);
+  const [isSimilar, setIsSimilar] = useState(false);
 
-  const [store, setStore] = useState('');
+  const [isSelectCenter, setIsSelectCenter] = useState(false);
+
+  const [center, setCenter] = useState('');
+  const [wall, setWall] = useState('');
+  const [level, setLevel] = useState('');
+  const [holdColor, setHoldColor] = useState('');
 
   function onSubmitSearch() {
     navigation.navigate('검색 결과');
@@ -68,85 +75,119 @@ function BoardTab({navigation}) {
 
   return (
     <>
-      <View style={styles.pickerBox}>
-        <Text style={styles.text}>지점</Text>
-        <View style={styles.pickerItem}>
-          <Picker
-            mode="dropdown"
-            dropdownIconColor="black"
-            selectedValue={store}
-            style={styles.picker}
-            onValueChange={(value, idx) => setStore(value)}>
-            <Picker.Item style={styles.text} label="Java" value="java" />
-            <Picker.Item style={styles.text} label="JavaScript" value="js" />
-          </Picker>
+      <View style={styles.selectContainer}>
+        <View style={styles.box}>
+          <Text style={styles.text}>지점</Text>
+          <View style={styles.pickerItem}>
+            <Picker
+              mode="dropdown"
+              dropdownIconColor="black"
+              selectedValue={center}
+              style={styles.picker}
+              onValueChange={(value, idx) => setCenter(value)}>
+              <Picker.Item
+                style={styles.pickerText}
+                label="Java"
+                value="java"
+              />
+              <Picker.Item
+                style={styles.pickerText}
+                label="JavaScript"
+                value="js"
+              />
+            </Picker>
+          </View>
+        </View>
+
+        <View style={styles.box}>
+          <Text style={styles.text}>구역</Text>
+          <View style={styles.pickerItem}>
+            <Picker
+              mode="dropdown"
+              dropdownIconColor="black"
+              selectedValue={wall}
+              style={styles.picker}
+              onValueChange={(value, idx) => setWall(value)}>
+              <Picker.Item
+                style={styles.pickerText}
+                label="Java"
+                value="java"
+              />
+              <Picker.Item
+                style={styles.pickerText}
+                label="JavaScript"
+                value="js"
+              />
+            </Picker>
+          </View>
+        </View>
+
+        <View style={styles.box}>
+          <Text style={styles.text}>난이도</Text>
+          <View style={styles.pickerItem}>
+            <Picker
+              mode="dropdown"
+              dropdownIconColor="black"
+              selectedValue={level}
+              style={styles.picker}
+              itemStyle={styles.item}
+              onValueChange={(value, idx) => setLevel(value)}>
+              {holdList.map(item => {
+                return (
+                  <Picker.Item
+                    key={item}
+                    style={styles.pickerText}
+                    label={item}
+                    value={item}
+                  />
+                );
+              })}
+            </Picker>
+          </View>
+        </View>
+
+        <View style={styles.box}>
+          <Text style={styles.text}>홀드 색상</Text>
+          <View style={styles.pickerItem}>
+            <Picker
+              // mode="dropdown"
+              dropdownIconColor="black"
+              selectedValue={holdColor}
+              style={styles.picker}
+              onValueChange={(value, idx) => setHoldColor(value)}>
+              {holdList.map(item => {
+                return (
+                  <Picker.Item
+                    key={item}
+                    style={styles.pickerText}
+                    label={item}
+                    value={item}
+                  />
+                );
+              })}
+            </Picker>
+          </View>
         </View>
       </View>
-
-      <View style={styles.pickerBox}>
-        <Text style={styles.text}>구역</Text>
-        <View style={styles.pickerItem}>
-          <Picker
-            mode="dropdown"
-            dropdownIconColor="black"
-            selectedValue={store}
-            style={styles.picker}
-            onValueChange={(value, idx) => setStore(value)}>
-            <Picker.Item style={styles.text} label="Java" value="java" />
-            <Picker.Item style={styles.text} label="JavaScript" value="js" />
-          </Picker>
-        </View>
-      </View>
-
-      <View style={styles.pickerBox}>
-        <Text style={styles.text}>난이도</Text>
-        <View style={styles.pickerItem}>
-          <Picker
-            mode="dropdown"
-            dropdownIconColor="black"
-            selectedValue={store}
-            style={styles.picker}
-            onValueChange={(value, idx) => setStore(value)}>
-            <Picker.Item style={styles.text} label="Java" value="java" />
-            <Picker.Item style={styles.text} label="JavaScript" value="js" />
-          </Picker>
-        </View>
-      </View>
-
-      <View style={styles.pickerBox}>
-        <Text style={styles.text}>홀드 색상</Text>
-        <View style={styles.pickerItem}>
-          <Picker
-            mode="dropdown"
-            dropdownIconColor="black"
-            selectedValue={store}
-            style={styles.picker}
-            onValueChange={(value, idx) => setStore(value)}>
-            <Picker.Item style={styles.text} label="Java" value="java" />
-            <Picker.Item style={styles.text} label="JavaScript" value="js" />
-          </Picker>
-        </View>
-      </View>
-
       <View style={styles.checkGroup}>
-        {isCheck ? (
+        {isSimilar ? (
           <TouchableOpacity
             onPress={() => {
-              setIsCheck(false);
+              setIsSimilar(false);
             }}>
             <Checked width={20} height={20} />
           </TouchableOpacity>
         ) : (
           <TouchableOpacity
             onPress={() => {
-              setIsCheck(true);
+              setIsSimilar(true);
             }}>
             <UnChecked width={20} height={20} />
           </TouchableOpacity>
         )}
         <TouchableOpacity
           onPress={() => {
-            setIsCheck(!isCheck);
+            setIsSimilar(!isSimilar);
           }}>
           <Text style={styles.checkText}>
             &nbsp; 나와 체형이 비슷한 사람의 결과만 보기
@@ -173,22 +214,6 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'center',
   },
-  button: {
-    display: 'flex',
-    flexDirection: 'row',
-    elevation: 8,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 10,
-    width: '80%',
-    height: 40,
-    backgroundColor: '#F34D7F',
-    margin: 5,
-    marginVertical: 15,
-  },
-  btnText: {
-    color: 'white',
-  },
   tabBox: {
     width: '100%',
     display: 'flex',
@@ -210,17 +235,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#F34D7F',
   },
-  checkGroup: {
-    display: 'flex',
-    flexDirection: 'row',
-    margin: 10,
+
+  selectContainer: {
+    width: '100%',
+    alignItems: 'center',
+    marginVertical: '15%',
   },
-  checkText: {
-    color: 'black',
-    fontSize: 14,
-    backgroundColor: 'white',
-  },
-  pickerBox: {
+  box: {
     width: '80%',
     display: 'flex',
     flexDirection: 'row',
@@ -233,16 +254,43 @@ const styles = StyleSheet.create({
     fontSize: 14,
   },
   pickerItem: {
-    backgroundColor: 'white',
+    width: '70%',
     borderBottomWidth: 1,
     borderColor: 'black',
-    width: '70%',
-    height: 50,
   },
-  picker: {},
+  picker: {
+    width: '100%',
+    color: 'black',
+  },
   pickerText: {
+    backgroundColor: 'white',
     color: 'black',
     fontSize: 14,
+  },
+  checkGroup: {
+    display: 'flex',
+    flexDirection: 'row',
+    margin: 10,
+  },
+  checkText: {
+    color: 'black',
+    fontSize: 14,
+    backgroundColor: 'white',
+  },
+  button: {
+    display: 'flex',
+    flexDirection: 'row',
+    elevation: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    width: '80%',
+    height: 40,
+    backgroundColor: '#F34D7F',
+    marginTop: '15%',
+  },
+  btnText: {
+    color: 'white',
   },
 });
 
