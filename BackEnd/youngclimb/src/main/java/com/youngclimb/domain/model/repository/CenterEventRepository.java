e1aacba11b873a8887b61242d78f9f284a399b47
김민성 <alstjd5255@naver.com>
2022-11-03 13:54:15 +0900

    fix: CenterDetail

--- BackEnd/youngclimb/src/main/java/com/youngclimb/domain/model/repository/CenterEventRepository.java
+++ BackEnd/youngclimb/src/main/java/com/youngclimb/domain/model/repository/CenterEventRepository.java
@@ -5,11 +5,14 @@ import com.youngclimb.domain.model.entity.CenterEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CenterEventRepository extends JpaRepository<CenterEvent, Integer> {
 
     Optional<CenterEvent> findByCenter(Center center);
+
+    List<CenterEvent> findAllByCenterOrderByDate(Center center);
 
 }
