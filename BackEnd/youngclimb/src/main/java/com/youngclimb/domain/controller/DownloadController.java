2de6c8691eb5616bae4642c2608477f1a8cd48b7
terrykim96 <terrykim96@naver.com>
2022-11-04 16:32:35 +0900

    feat: 다운로드 수 증가

---
+++ BackEnd/youngclimb/src/main/java/com/youngclimb/domain/controller/DownloadController.java
@@ -0,0 +1,34 @@
+package com.youngclimb.domain.controller;
+
+import com.youngclimb.domain.model.service.DownloadService;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/download")
+@RequiredArgsConstructor
+public class DownloadController {
+    private final DownloadService downloadService;
+
+    // 다운로드 수 업데이트
+    @ApiOperation(value = "updateDownload : 다운로드 수 업데이트")
+    @PostMapping
+    public ResponseEntity<?> updateDownload() throws Exception {
+        try {
+            return new ResponseEntity<Integer>(downloadService.updateDownload(), HttpStatus.OK);
+
+        } catch (Exception e) {
+            return exceptionHandling(e);
+        }
+    }
+
+    private ResponseEntity<String> exceptionHandling(Exception e) {
+        e.printStackTrace();
+        return new ResponseEntity<String>("Error : " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
