ce28351226781a634429b0a631d6ffc6cccf7edf
gunseon.jin <gunseon.jin@gmail.com>
2022-11-10 17:49:23 +0900

    [BE] fix: refreshToken reIssue

--- BackEnd/youngclimb/src/main/java/com/youngclimb/domain/model/service/MemberServiceImpl.java
+++ BackEnd/youngclimb/src/main/java/com/youngclimb/domain/model/service/MemberServiceImpl.java
@@ -7,7 +7,6 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.youngclimb.common.exception.ResourceNotFoundException;
 import com.youngclimb.common.jwt.JwtTokenProvider;
 import com.youngclimb.domain.model.dto.board.NoticeDto;
-import com.youngclimb.domain.model.dto.center.CenterDto;
 import com.youngclimb.domain.model.dto.member.*;
 import com.youngclimb.domain.model.entity.*;
 import com.youngclimb.domain.model.repository.*;
@@ -49,6 +48,7 @@ public class MemberServiceImpl implements MemberService {
     private final MemberProblemRepository memberProblemRepository;
     private final NoticeRepository noticeRepository;
     private final AmazonS3 amazonS3;
+
 
 
     // 이메일 중복 체크
@@ -219,7 +219,6 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public void verifyUser(String email, String password) {
-
     }
 
     @Override
@@ -229,7 +228,6 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public void deleteMember(String email) {
-
 
     }
 
@@ -247,9 +245,6 @@ public class MemberServiceImpl implements MemberService {
                 .accessToken(jwtTokenProvider.createAccessToken(member.getEmail()))
                 .refreshToken(jwtTokenProvider.createRefreshToken(member.getEmail()))
                 .build();
-
-
-//        memberRankExp.getRank().getProblem();
 
         MemberProblem memberProblem = memberProblemRepository.findByMember(loginMember).orElseThrow();
 
