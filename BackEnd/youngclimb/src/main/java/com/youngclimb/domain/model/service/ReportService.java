5094be8b5f5c492638d1abc4fecb43e9c5ad9b68
terrykim96 <terrykim96@naver.com>
2022-11-13 17:36:29 +0900

    feat: admin 페이지 api 구현(10%) / fix: 댓글 생성 api 수정

---
+++ BackEnd/youngclimb/src/main/java/com/youngclimb/domain/model/service/ReportService.java
@@ -0,0 +1,9 @@
+package com.youngclimb.domain.model.service;
+
+import com.youngclimb.domain.model.dto.ReportDto;
+
+import java.util.List;
+
+public interface ReportService {
+//    List<ReportDto> readReport(String email);
+}
