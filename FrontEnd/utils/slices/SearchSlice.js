547d23735b361e093af9ea5a461fefa6f2ff0c46
DK <ehdrms121@naver.com>
2022-11-15 17:23:24 +0900

    feat: 프로필 페이지에서 게시글 삭제 구현

--- FrontEnd/utils/slices/SearchSlice.js
+++ FrontEnd/utils/slices/SearchSlice.js
@@ -1,5 +1,4 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
-import axios from 'axios';
 import axiosTemp from '../axios';
 import api from '../api';
 import getConfig from '../headers';
@@ -7,10 +6,8 @@ import getConfig from '../headers';
 const fetchUser = createAsyncThunk(
   'fetchUser',
   async (arg, {rejectWithValue}) => {
-    console.log('추천 유저 받기');
     try {
       const res = await axiosTemp.get(api.searchUser(), await getConfig());
-      console.log('추천유저정보', res.data);
       return res.data;
     } catch (err) {
       return rejectWithValue(err.response.data);
@@ -20,7 +17,6 @@ const fetchUser = createAsyncThunk(
 
 const search = createAsyncThunk('search', async (data, {rejectWithValue}) => {
   try {
-    console.log('검색 요청', data);
     const res = await axiosTemp.post(api.search(), data, await getConfig());
     return res.data;
   } catch (err) {
@@ -31,17 +27,14 @@ const search = createAsyncThunk('search', async (data, {rejectWithValue}) => {
 const searchUser = createAsyncThunk(
   'searchUser',
   async (data, {rejectWithValue}) => {
-    console.log('유저 검색 요청');
     try {
       const res = await axiosTemp.post(
         api.searchUser(),
         data,
         await getConfig(),
       );
-      console.log(res.data);
       return res.data;
     } catch (err) {
-      console.log(err);
       return rejectWithValue(err.response.data);
     }
   },
@@ -64,17 +57,14 @@ export const SearchSlice = createSlice({
     [search.fulfilled]: (state, action) => {
       state.boards = action.payload;
     },
-    [search.rejected]: (state, action) => {
-      console.log('검색 실패');
+    [search.rejected]: state => {
       state.boards = [];
     },
     [searchUser.fulfilled]: (state, action) => {
-      console.log('유저검색 성공');
       state.users = action.payload;
     },
-    [searchUser.rejected]: (state, action) => {
+    [searchUser.rejected]: state => {
       state.users = [];
-      console.log('검색 실패');
     },
   },
 });
