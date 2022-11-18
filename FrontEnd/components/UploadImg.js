5a9c5399221ebe6ebbe561d0a76dca6c1dae917d
DK <ehdrms121@naver.com>
2022-11-18 23:20:59 +0900

    refactor: 윙스팬 예시사진 변경

--- FrontEnd/components/UploadImg.js
+++ FrontEnd/components/UploadImg.js
@@ -1,15 +1,22 @@
 /* eslint-disable no-undef */ // for Platform.OS
-import React, { useState } from 'react';
+import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {View, Text, Image, StyleSheet, TouchableOpacity, PermissionsAndroid} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  PermissionsAndroid,
+} from 'react-native';
 import {launchImageLibrary, launchCamera} from 'react-native-image-picker';
 
 import {changeUploadImg} from '../utils/slices/AccountsSlice';
 
-import wingspanExample from '../assets/image/main/wingspanExample.png';
+import wingspanExample from '../assets/image/main/wingspanExample.jpg';
 import gallery from '../assets/image/main/whiteGallery.png';
 import camera from '../assets/image/main/whiteCamera.png';
-import { checkPermission, requestSinglePermission } from '../utils/permissions';
+import {checkPermission, requestSinglePermission} from '../utils/permissions';
 
 const imagePickerOption = {
   storageOptions: {
@@ -28,39 +35,52 @@ function UploadImg() {
   const uploadImg = useSelector(state => state.accounts.uploadImg);
 
   const onGallery = () => {
-    checkPermission(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE).then((res)=>{
-      const granted = res
-      console.log(res)
-      if (!granted){
-        const message = {title:'권한 거부된 요청 (저장공간)', content:'서비스 이용을 위한 권한 요청이 거부되어 설정에서 권한 설정 후 앱 사용바랍니다.'}
-        requestSinglePermission(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,message)
-      } else {
-        launchImageLibrary(
-          {
-            mediaType: 'photo',
-            maxWidth: 512,
-            maxHeight: 512,
-            includeBase64: Platform.OS === 'android',
-          },
-          res => {
-            if (res.didCancel) {
-              return;
-            }
-    
-            dispatch(changeUploadImg(res));
-          },
-      );
-      }
-    })
+    checkPermission(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE).then(
+      res => {
+        const granted = res;
+        console.log(res);
+        if (!granted) {
+          const message = {
+            title: '권한 거부된 요청 (저장공간)',
+            content:
+              '서비스 이용을 위한 권한 요청이 거부되어 설정에서 권한 설정 후 앱 사용바랍니다.',
+          };
+          requestSinglePermission(
+            PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+            message,
+          );
+        } else {
+          launchImageLibrary(
+            {
+              mediaType: 'photo',
+              maxWidth: 512,
+              maxHeight: 512,
+              includeBase64: Platform.OS === 'android',
+            },
+            res => {
+              if (res.didCancel) {
+                return;
+              }
+
+              dispatch(changeUploadImg(res));
+            },
+          );
+        }
+      },
+    );
   };
 
   const onCamera = () => {
-    checkPermission(PermissionsAndroid.PERMISSIONS.CAMERA).then((res)=>{
-      const granted = res
-      console.log(res)
-      if (!granted){
-        const message = {title:'권한 거부된 요청 (카메라)', content:'서비스 이용을 위한 권한 요청이 거부되어 설정에서 권한 설정 후 앱 사용바랍니다.'}
-        requestSinglePermission(PermissionsAndroid.PERMISSIONS.CAMERA,message)
+    checkPermission(PermissionsAndroid.PERMISSIONS.CAMERA).then(res => {
+      const granted = res;
+      console.log(res);
+      if (!granted) {
+        const message = {
+          title: '권한 거부된 요청 (카메라)',
+          content:
+            '서비스 이용을 위한 권한 요청이 거부되어 설정에서 권한 설정 후 앱 사용바랍니다.',
+        };
+        requestSinglePermission(PermissionsAndroid.PERMISSIONS.CAMERA, message);
       } else {
         launchCamera(imagePickerOption).then(res => {
           if (res.didCancel || !res) {
@@ -69,9 +89,7 @@ function UploadImg() {
           dispatch(changeUploadImg(res));
         });
       }
-    })
-
-
+    });
   };
 
   return (
