fea0ae0dabced8a676c8b18a945da3e5408752f2
dongkeun2 <ehdrms121@ajou.ac.kr>
2022-11-24 07:10:44 +0900

    fix: alert창 한 번만

--- front-webview/src/pages/Admin.jsx
+++ front-webview/src/pages/Admin.jsx
@@ -1,36 +1,29 @@
-import React from "react";
-import {
-  Navigate,
-  Route,
-  Routes,
-  useLocation,
-  useNavigate,
-} from "react-router-dom";
-import { useSelector } from "react-redux";
+import React from 'react'
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
-import AdminHome from "./admin/AdminHome";
-import AdminLogin from "./admin/AdminLogin";
-import AdminReportList from "./admin/AdminReportList";
-import AdminReportDetail from "./admin/AdminReportDetail";
-import AdminStore from "./admin/AdminStore";
+import AdminHome from './admin/AdminHome'
+import AdminLogin from './admin/AdminLogin'
+import AdminReportDetail from './admin/AdminReportDetail'
+import AdminStore from './admin/AdminStore'
 
-import "../App.css";
-import { useEffect } from "react";
-import AdminUser from "./admin/AdminUser";
+import '../App.css'
+import { useEffect } from 'react'
+import AdminUser from './admin/AdminUser'
 
 const Admin = () => {
-  const location = useLocation();
-  const isAuthenticated = useSelector((state) => state.authToken.authenticated);
-  const navigate = useNavigate();
+  const location = useLocation()
+  const isAuthenticated = useSelector(state => state.authToken.authenticated)
+  const navigate = useNavigate()
   useEffect(() => {
     if (isAuthenticated) {
-      navigate("/admin/report");
+      navigate('/admin/report')
     }
-    if (!isAuthenticated && location.pathname !== "/admin/login") {
-      navigate("/admin/login");
-      alert("로그인 후 사용 가능합니다.\n관리자 계정으로 로그인바랍니다.");
+    if (!isAuthenticated && window.location.pathname !== '/admin/login') {
+      alert('로그인 후 사용 가능합니다.\n관리자 계정으로 로그인바랍니다.')
+      navigate('/admin/login')
     }
-  }, []);
+  }, [])
 
   return (
     <div className="adminBox">
@@ -49,7 +42,7 @@ const Admin = () => {
         <Route path="*" element={<AdminHome />} />
       </Routes>
     </div>
-  );
-};
+  )
+}
 
-export default Admin;
+export default Admin
